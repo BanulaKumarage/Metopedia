8d4fde4b0c7c1ba44618616ac7ec3e3623c42eb2
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2023-10-23 00:42:25 +0530

    UI enhancements

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-import SearchBox from './components/Searchbar';
-import MetaphorsTable from './components/Table';
+import logo from "./logo.svg";
+import "./App.css";
+import SearchBox from "./components/Searchbar";
+import MetaphorsTable from "./components/Table";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Container } from '@mui/material';
+import { Container } from "@mui/material";
+import { Select } from "@mui/material";
+import { MenuItem, Grid } from "@mui/material";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 function App() {
   const [phrase, setPhrase] = useState("");
-  const [data, setData] = useState([])
-  
+  const [data, setData] = useState([]);
+  const [mode, setMode] = useState("search");
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+
   useEffect(() => {
-      const request = {phrase: phrase};
-      axios.post(`http://localhost:3001/search`, request).then((result) => {
-        setData(result.data);
-      });
-    }, [phrase])
+    const request = { phrase: phrase };
+    axios.post(`http://localhost:3001/${mode}`, request).then((result) => {
+      console.log(`Mode: ${mode}`);
+      setData(result.data);
+    });
+  }, [phrase]);
 
   return (
-    <Container className="App">
-      <SearchBox placeholder="Search Elasticsearch" setQueryText={setPhrase}/>
-      <MetaphorsTable data={data.hits} />
-    </Container>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container className="App">
+        <Grid container display={"flex"} justifyContent={"center"}>
+          <Grid item xs={4}>
+            <SearchBox placeholder="Search" setQueryText={setPhrase} />
+          </Grid>
+          <Grid item xs={2} display={"flex"} alignItems={"center"}>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={mode}
+              label="mode"
+              onChange={(e)=>{setMode(e.target.value)}}
+            >
+            <MenuItem value={'search'}>All</MenuItem>
+            <MenuItem value={'author'}>Author</MenuItem>
+            <MenuItem value={'poem'}>Poem</MenuItem>
+            <MenuItem value={'source'}>Source Domain</MenuItem>
+            <MenuItem value={'target'}>Target Domain</MenuItem>
+            </Select>
+          </Grid>
+        </Grid>
+        <MetaphorsTable data={data.hits} />
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,17 +1,33 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TablePagination from "@mui/material/TablePagination";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
 export default function MetaphorsTable({ data }) {
-  return (
-    data && data.length!= 0 && <TableContainer component={Paper}>
-      <Typography fontSize={13} fontStyle={'italic'}>Number of results: {data.length}</Typography>
+  
+  const [pg, setpg] = React.useState(0);
+  const [rpg, setrpg] = React.useState(5);
+
+  function handleChangePage(event, newpage) {
+    setpg(newpage);
+  }
+
+  function handleChangeRowsPerPage(event) {
+    setrpg(parseInt(event.target.value, 10));
+    setpg(0);
+  }
+
+  return data && data.length != 0 ? (
+    <TableContainer component={Paper}>
+      <Typography fontSize={13} fontStyle={"italic"}>
+        Number of results: {data.length}
+      </Typography>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -25,24 +41,43 @@ export default function MetaphorsTable({ data }) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((metaphor) => (
+          {data.slice(pg * rpg, pg * rpg + rpg).map((metaphor) => (
             <TableRow
               key={metaphor._id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {metaphor._source.poem_name}
               </TableCell>
               <TableCell align="right">{metaphor._source.poet}</TableCell>
               <TableCell align="right">{metaphor._source.line}</TableCell>
-              <TableCell align="right">{metaphor._source.metaphorical_terms}</TableCell>
-              <TableCell align="right">{metaphor._source.source_domain}</TableCell>
-              <TableCell align="right">{metaphor._source.target_domain}</TableCell>
+              <TableCell align="right">
+                {metaphor._source.metaphorical_terms}
+              </TableCell>
+              <TableCell align="right">
+                {metaphor._source.source_domain}
+              </TableCell>
+              <TableCell align="right">
+                {metaphor._source.target_domain}
+              </TableCell>
               <TableCell align="right">{metaphor._source.meaning}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 30]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rpg}
+        page={pg}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </TableContainer>
+  ) : (data==null || data.length==0) && (
+    <Typography fontSize={13} fontStyle={"italic"}>
+      No results found
+    </Typography>
   );
 }
